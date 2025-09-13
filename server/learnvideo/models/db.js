57c2d3b19bc8202e1e06e6686f164e3589b645ea
Ntoils <rredqwerty@gmail.com>
2025-09-13 20:10:47 +0300

    add register and login endpoints

---
+++ server/learnvideo/models/db.js
@@ -0,0 +1,5 @@
+export const UsersDB = [
+    {id: 1 , username: "ruslan", password: "12345", role: "admin"},
+    {id: 2 , username: "alena", password: "12345", role: "user"},
+    {id: 3 , username: "kamilla", password: "$2b$10$zu9xmcv61VVM3lNpEk.Cd.VJVspN43C93GCptxkR9Nh/UAtgj137G", role: "admin"},
+]
